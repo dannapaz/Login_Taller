5f5707b312cff1e19acbe392d97c2334b118eac9
dannapaz <dannapazlopez@gmail.com>
2024-11-07 16:29:44 -0500

    Se agrego nuevo test

--- Login_Taller/LoginTest.cs
+++ Login_Taller/LoginTest.cs
@@ -26,8 +26,30 @@ namespace Login_Taller
             driver.Close();
             //cerrar el driver
             driver.Quit();
+        }
 
+        public void IngresoIncorrecto()
+        {
+            //Driver: Es el que se comunica con el navegador
 
+            IWebDriver driver = new ChromeDriver();
+            //Maximizar el navegador
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("https://the-internet.herokuapp.com/login");
+
+            //Localizadores
+            //Identificación de los elementos. 
+
+            driver.FindElement(By.Id("username")).SendKeys("tomsmith1");
+            driver.FindElement(By.Id("password")).SendKeys("SuperSecretPassword!");
+            driver.FindElement(By.CssSelector("#login button")).Click();
+           //Para visualizar el test mas lento
+            Thread.Sleep(500);
+
+            //cerrar el navegador
+            driver.Close();
+            //cerrar el driver
+            driver.Quit();
 
         }
     }
