ae71198ddfa183cc8de05631e3e41d434b6b00fe
dannapaz <dannapazlopez@gmail.com>
2024-11-07 17:04:36 -0500

    Se agregar cambios

--- Login_Taller/LoginTest.cs
+++ Login_Taller/LoginTest.cs
@@ -1,56 +1,47 @@
+using Login_Taller.PageObject;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using static System.Net.WebRequestMethods;
 
 namespace Login_Taller
 {
+
+    //Driver: Es el que se comunica con el navegador
+    //POM sirve para aislar los elementos 
+    //Localizadores: Identificación de los elementos. 
+    //Para visualizar el test mas lento Thread.Sleep(500);
+
     public class Tests
     {
+
+        public IWebDriver driver;
+        public LoginPage login;
+        public String baseUrl = "https://the-internet.herokuapp.com/login";
+
+        [SetUp]
+        public void IniciarNavegador() {
+
+            //Maximizar el navegador
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl(baseUrl);
+            login = new LoginPage(driver);
+
+        }
+        [TearDown]
+        public void cerrarNavegador()
+        {
+            //cerrar el navegador
+            driver.Close();
+            //cerrar el driver
+            driver.Quit();
+        }
         [Test]
         public void IngresoCorrecto()
         {
-        //Driver: Es el que se comunica con el navegador
-
-          IWebDriver driver= new ChromeDriver();
-            //Maximizar el navegador
-            driver.Manage().Window.Maximize();
-            driver.Navigate().GoToUrl("https://the-internet.herokuapp.com/login");
-
-            //Localizadores
-            //Identificación de los elementos. 
-
-            driver.FindElement(By.Id("username")).SendKeys("tomsmith");
-            driver.FindElement(By.Id("password")).SendKeys("SuperSecretPassword!");
-            driver.FindElement(By.CssSelector("#login button")).Click();
-
-            //cerrar el navegador
-            driver.Close();
-            //cerrar el driver
-            driver.Quit();
+            login.IngresarCredenciales();
+            
         }
 
-        public void IngresoIncorrecto()
-        {
-            //Driver: Es el que se comunica con el navegador
-
-            IWebDriver driver = new ChromeDriver();
-            //Maximizar el navegador
-            driver.Manage().Window.Maximize();
-            driver.Navigate().GoToUrl("https://the-internet.herokuapp.com/login");
-
-            //Localizadores
-            //Identificación de los elementos. 
-
-            driver.FindElement(By.Id("username")).SendKeys("tomsmith1");
-            driver.FindElement(By.Id("password")).SendKeys("SuperSecretPassword!");
-            driver.FindElement(By.CssSelector("#login button")).Click();
-           //Para visualizar el test mas lento
-            Thread.Sleep(500);
-
-            //cerrar el navegador
-            driver.Close();
-            //cerrar el driver
-            driver.Quit();
-
-        }
     }
 }
---
+++ Login_Taller/PageObject/LoginPage.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+
+namespace Login_Taller.PageObject
+{
+    public class LoginPage
+    {
+        public IWebDriver _driver;
+        public LoginPage(IWebDriver driver)
+        {
+            this._driver = driver;
+        }
+
+        //readonly: No se puede cambiar el valor durante la ejecución 
+
+        private readonly By _txtUsername = By.Id("username");
+        private readonly By _txtPassword = By.Id("password");
+        private readonly By _btnLogin = By.CssSelector("#login button");
+
+        public IWebElement username => _driver.FindElement(_txtUsername);
+        public IWebElement password => _driver.FindElement(_txtPassword);
+        public IWebElement botonLogin => _driver.FindElement(_btnLogin);
+
+        public void IngresarCredenciales()
+        {
+            username.SendKeys("tomsmith");
+            password.SendKeys("SuperSecretPassword!");
+            botonLogin.Click();
+        }
+    }
+}
